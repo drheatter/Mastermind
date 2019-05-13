fdd3d4a7c0e0c8b090ff97604fb62923ae4fb6a1
Doug Heatter <drheatter@gmail.com>
2019-05-13 15:05:47 -0400

    Added methods to Code class to check code guesses.

--- mastermind.rb
+++ mastermind.rb
@@ -14,7 +14,32 @@ class Code
 	def initialize(code)
 		@code = code
 	end
+
+	def is_correct?(guess)
+		@code == guess ? true : false
+	end
+
+	def check_guess(guess)
+		guess_char_array = guess.split('')
+		return_string = ''
+		guess_char_array.each_index do |i|
+			if guess_char_array[i] == @code[i]
+				return_string += guess_char_array[i].green
+			elsif @code.include?(guess_char_array[i])
+				return_string += guess_char_array[i]
+			else
+				return_string += guess_char_array[i].red
+			end
+		end
+		return_string
+	end
 end
 
-puts "banana".red
-puts "orange".green
+
+#test stuff
+test_code = Code.new('1234')
+puts test_code.check_guess('1234')
+puts test_code.check_guess('1111')
+puts test_code.check_guess('6526')
+puts test_code.is_correct?('1234')
+puts test_code.is_correct?('1222')
