95a65288d1b8e804df73dbbe35c7c1b31a208541
Doug Heatter <drheatter@gmail.com>
2019-05-13 14:41:05 -0400

    Added methods to String class to allow colored text.

--- mastermind.rb
+++ mastermind.rb
@@ -1,6 +1,20 @@
+class String
+
+	def red
+		"\e[31m#{self}\e[0m"
+	end
+
+	def green;
+		"\e[32m#{self}\e[0m" 
+	end
+end
+
 class Code
 
 	def initialize(code)
 		@code = code
 	end
 end
+
+puts "banana".red
+puts "orange".green
