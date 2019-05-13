359f14e7ed97ac5de0520d8037818deb67efa816
Doug Heatter <drheatter@gmail.com>
2019-05-13 15:41:37 -0400

    Added get_guess method to Game class.

--- mastermind.rb
+++ mastermind.rb
@@ -46,12 +46,48 @@ class ComputerPlayer
 
 	def generate_code()
 		code = ""
-		4.times { code += (1 + rand(6)).to_s}
+		4.times { code += (1 + rand(6)).to_s }
 		code
+	end
+end
+
+class Game
+
+	def initialize
+		@cpu_player = ComputerPlayer.new
+	end
+
+	def play
+		print_instructions
+		4.times do
+			guess = get_guess
+		end
+	end
+
+	private
+
+	def print_instructions
+		puts "Welcome to Mastermind!" 
+		puts "The computer will think of a code of four digits between 1 and 6."
+		puts "Your challenge is to guess it in four tries."
+		puts "After each guess, you will see a response."
+		puts "Green numbers in this response are correct."
+		puts "White numbers are in the code, but in a different position."
+		puts "Red numbers aren't in the code at all."
+		puts "Now, let's play!"
+	end
+
+	def get_guess
+		loop do
+			puts "Guess a code (four digits between 1 and 6)"
+			input = gets.chomp
+			return input if input.match(/^[1-6]{4}$/)
+			puts "Invalid input, try again."
+		end
 	end
 end
 
 
 #test stuff
-test_cpu = ComputerPlayer.new
-puts test_cpu.code
+test_game = Game.new
+test_game.play
